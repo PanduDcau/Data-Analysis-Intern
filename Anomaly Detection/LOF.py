957c938c8523ec681a4435450e053d1b0e17f3cb
Pandula <pandula@tsdclanka.com>
2021-11-16 08:51:08 +0530

    Anomaly updated

--- Anomaly Detection/LOF.py
+++ Anomaly Detection/LOF.py
@@ -7,6 +7,12 @@ from pyod.models.knn import KNN
 import json
 from flask import send_from_directory
 from flask import current_app
+import matplotlib.pyplot as plt
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import StandardScaler
+from mpl_toolkits.mplot3d import Axes3D
+from sklearn.covariance import EllipticEnvelope
+from sklearn.neighbors import LocalOutlierFactor
 
 app = Flask(__name__)
 
@@ -24,14 +30,55 @@ class Lof:
         df = pd.DataFrame(self.file)
         non_num=pd.DataFrame(self.non_num)
         df.dropna(axis = 0, inplace = True)
-        # x = df.select_dtypes(include = ['float64','int64'])
-        from sklearn.neighbors import LocalOutlierFactor
-        lof = LocalOutlierFactor()
-        clusters = lof.fit_predict(df)
-        anom_index = where(clusters == -1)
-        values = df.iloc[anom_index]
-        values = non_num.join(values, how='inner')
-        values.to_csv(r'LOF_outliers.csv' ,index=True,header=True)
+        x=len(df.columns)
+        if x==1:
+            # x = df.select_dtypes(include = ['float64','int64'])
+            lof = LocalOutlierFactor()
+            clusters = lof.fit_predict(df)
+            anom_index = where(clusters == -1)
+            values = df.iloc[anom_index]
+            values = non_num.join(values, how='inner')
+            values.to_csv(r'LOF_outliers.csv' ,index=True,header=True)
+
+        else:
+            lof = LocalOutlierFactor()
+            clusters = lof.fit_predict(df)
+            anom_index = where(clusters == -1)
+            values = df.iloc[anom_index]
+            outlier_index=list(values.index)
+
+            pca = PCA(n_components=2)  # Reduce to k=3 dimensions
+            scaler = StandardScaler()
+            #normalize the metrics
+            X = scaler.fit_transform(df)
+            X_reduce = pca.fit_transform(X)
+
+            ## 3D
+
+            fig = plt.figure()
+            ax = fig.add_subplot(111, projection='3d')
+            ax.set_zlabel("x_composite_3")
+            # Plot the compressed data points
+            ax.scatter(X_reduce[:, 0], X_reduce[:, 1], s=4, lw=1, label="inliers",c="green")
+            # Plot x's for the ground truth outliers
+            ax.scatter(X_reduce[outlier_index,0],X_reduce[outlier_index,1], lw=2, s=60, marker="x", c="red", label="outliers")
+            ax.legend()
+            plt.show()
+            plt.savefig('D:\\Local Disk D\\Anomaly New Detect\\static\\img\\LOF_3D.png')
+
+            ### 2D ####
+
+            fig, ax = plt.subplots(figsize=(16,8))
+            ax.scatter(X_reduce[:, 0], X_reduce[:, 1],s=4, lw=1, label="inliers",c="green")
+            ax.scatter(X_reduce[outlier_index,0],X_reduce[outlier_index,1],lw=2, s=60, marker="x", c="red", label="outliers")
+            ax.legend()
+            plt.show()
+            plt.savefig('D:\\Local Disk D\\Anomaly New Detect\\static\\img\\LOF_2D.png')
+
+
+            values = non_num.join(values, how='inner')
+            values.to_csv(r'LOF_outliers.csv',index=True,header=True)
+
 
         # Local Outlier Factor Method
 
--- Anomaly Detection/MCD.py
+++ Anomaly Detection/MCD.py
@@ -7,6 +7,11 @@ from pyod.models.knn import KNN
 import json
 from flask import send_from_directory
 from flask import current_app
+import matplotlib.pyplot as plt
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import StandardScaler
+from mpl_toolkits.mplot3d import Axes3D
+from sklearn.covariance import EllipticEnvelope
 
 app = Flask(__name__)
 
@@ -21,14 +26,50 @@ class Mcd:
         data=pd.DataFrame(self.file)
         non_num=pd.DataFrame(self.non_num)
         data.dropna(axis=0,inplace=True)
-        # data=data.select_dtypes(include=['float64','int64'])
-        from sklearn.covariance import EllipticEnvelope
-        model  =  EllipticEnvelope(contamination=0.01).fit(data)
-        data['anomaly_score'] = model.predict(data)
-        anomaly=data[data['anomaly_score']==-1]
-        anomaly = non_num.join(anomaly, how='inner')
-        anomaly.to_csv("outlierss_mcd.csv")
+        x=len(data.columns)
+        if x==1:
+            # data_n=data_n.select_dtypes(include=['float64','int64'])
+            
+            model  =  EllipticEnvelope(contamination=0.01).fit(data)
+            data['anomaly_score'] = model.predict(data)
+            anomaly=data[data['anomaly_score']==-1]
+            outlier_index=list(anomaly.index)
+            anomaly = non_num.join(anomaly, how='inner')
+            anomaly.to_csv("outlierss_mcd.csv")
+                
+            
+        #### Plot the outliers
 
-        # Minimum covariance determinant Method
+        
+        else:
+            model  =  EllipticEnvelope(contamination=0.01).fit(data)
+            data['anomaly_score'] = model.predict(data)
+            anomaly=data[data['anomaly_score']==-1]
+            outlier_index=list(anomaly.index)
+            pca = PCA(n_components=3)  # Reduce to k=3 dimensions
+            scaler = StandardScaler()
+            #normalize the metrics
+            X = scaler.fit_transform(data)
+            X_reduce = pca.fit_transform(X)
+            fig = plt.figure()
+            ax = fig.add_subplot(111, projection='3d')
+            ax.set_zlabel("x_composite_3")
+            # Plot the compressed data points
+            ax.scatter(X_reduce[:, 0], X_reduce[:, 1], zs=X_reduce[:, 2], s=4, lw=1, label="inliers",c="green")
+            # Plot x's for the ground truth outliers
+            ax.scatter(X_reduce[outlier_index,0],X_reduce[outlier_index,1], X_reduce[outlier_index,2],lw=2, s=60, marker="x", c="red", label="outliers")
+            ax.legend()
+            plt.show()
+            plt.savefig('D:\\Local Disk D\\Anomaly New Detect\\static\\img\\MCD_3D.png')
 
-   
+            fig, ax = plt.subplots(figsize=(16,8))
+            ax.scatter(X_reduce[:, 0], X_reduce[:, 1],s=4, lw=1, label="inliers",c="green")
+            ax.scatter(X_reduce[outlier_index,0],X_reduce[outlier_index,1],lw=2, s=60, marker="x", c="red", label="outliers")
+            ax.legend()
+            plt.show()
+            plt.savefig('D:\\Local Disk D\\Anomaly New Detect\\static\\img\\MCD_2D.png')
+
+            anomaly = non_num.join(anomaly, how='inner')
+            anomaly.to_csv("outlierss_isolation.csv")
+        
+        # Minimum Covariance Determinant Method
