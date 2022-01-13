57bbc5b2166a70692019110f2cc3db029123e67b
Pandula <pandula@tsdclanka.com>
2022-01-13 12:51:06 +0530

    NLP Dashboard

--- NLP DashBoard/src/app/shared/components/sidebar/sidebar.component.ts
+++ NLP DashBoard/src/app/shared/components/sidebar/sidebar.component.ts
@@ -4,7 +4,7 @@ import {FormControl} from '@angular/forms';
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
-  styleUrls: ['./sidebar.component.scss','./sidebar.component.css']
+  styleUrls: ['./sidebar.component.scss']
 })
 
 
